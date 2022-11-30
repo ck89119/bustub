7125997fd86b09c28c26c1fd8986c0fe04e52d7c
Cao Kai <ck89119@gmail.com>
2022-11-30 10:26:26 +0100

    lru_k_replacer complete

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -14,16 +14,91 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+  accesss_history_ = std::vector<std::list<size_t>>(num_frames);
+  evictable_ = std::vector<bool>(num_frames);
+}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::unique_lock<std::mutex> l(latch_);
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+  size_t current_max_diff = 0;
+  size_t current_max_diff_timestamp = 0;
+  bool status = false;
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+  for (size_t i = 0; i < replacer_size_; ++i) {
+    if (accesss_history_[i].empty() || !evictable_[i]) {
+      continue;
+    }
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+    auto [diff, timestamp] = GetDiff(i);
+    if (diff > current_max_diff || (diff == current_max_diff && timestamp < current_max_diff_timestamp)) {
+      *frame_id = i;
+      current_max_diff = diff;
+      current_max_diff_timestamp = timestamp;
+      status = true;
+    }
+  }
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+  if (status) {
+    accesss_history_[*frame_id].clear();
+  }
+
+  return status;
+}
+
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+  std::unique_lock<std::mutex> l(latch_);
+
+  BUSTUB_ASSERT((size_t)frame_id < replacer_size_, "larger than replacer_size_");
+
+  auto &frame_accesss_history = accesss_history_[frame_id];
+  frame_accesss_history.push_back(current_timestamp_++);
+  if (frame_accesss_history.size() > k_) {
+    frame_accesss_history.pop_front();
+  }
+}
+
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  std::unique_lock<std::mutex> l(latch_);
+
+  BUSTUB_ASSERT((size_t)frame_id < replacer_size_, "larger than replacer_size_");
+
+  evictable_[frame_id] = set_evictable;
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::unique_lock<std::mutex> l(latch_);
+
+  BUSTUB_ASSERT((size_t)frame_id < replacer_size_, "larger than replacer_size_");
+
+  if (accesss_history_[frame_id].empty()) {
+    return;
+  }
+
+  BUSTUB_ASSERT(evictable_[frame_id], "non-evictable frame");
+
+  accesss_history_[frame_id].clear();
+}
+
+auto LRUKReplacer::Size() -> size_t {
+  std::unique_lock<std::mutex> l(latch_);
+
+  auto size = 0;
+  for (size_t i = 0; i < replacer_size_; ++i) {
+    if (!accesss_history_[i].empty() && evictable_[i]) {
+      size += 1;
+    }
+  }
+  return size;
+}
+
+auto LRUKReplacer::GetDiff(frame_id_t frame_id) -> std::pair<size_t, size_t> {
+  auto recent_k_timestamp = accesss_history_[frame_id].front();
+  if (accesss_history_[frame_id].size() < k_) {
+    return std::make_pair(0xffffffffffffffffULL, recent_k_timestamp);
+  }
+  return std::make_pair(current_timestamp_ - recent_k_timestamp, recent_k_timestamp);
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -16,6 +16,7 @@
 #include <list>
 #include <mutex>  // NOLINT
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 #include "common/config.h"
@@ -135,11 +136,15 @@ class LRUKReplacer {
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
+  size_t current_timestamp_{0};
+  // size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
+  std::vector<std::list<size_t>> accesss_history_;
+  std::vector<bool> evictable_;
   std::mutex latch_;
+
+  auto GetDiff(frame_id_t frame_id) -> std::pair<size_t, size_t>;
 };
 
 }  // namespace bustub
